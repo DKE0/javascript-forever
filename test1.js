0558e0e785f6b035a5670cb7f8fcfb560041f87f
DANVIDE Kodjo Espoir <danvidekodjoespoir@gmail.com>
2018-03-11 16:15:29 +0100

    code corrigé

--- test1.js
+++ test1.js
@@ -142,42 +142,37 @@ var special=['onze', 'douze','treize','quartoze', 'quinze','seize','dix-sept', '
 
 function converter(unit, decade, hundred)
 {
-   var numberconverted; 
-   if (usrnb > 100)
+   var usrnb2 = parseInt(usrnb,10);
+   var numberconverted;
+	// if usrnb2 digit number = 3
+   if (usrnb2 > 100)
    {
-   	  if (hundred)
-      {
-      	//alert(hundred);
      	if (hundred === 1) numberconverted = 'cent';
    	    else numberconverted = digit[hundred-1] + ' cent';
-      }
 
       if (decade)
       {
       	if (decade === 7) numberconverted +=' '+dizaine[5]+'-';
       	if (decade === 8 || decade === 9) numberconverted +=' '+digit[3]+'-'+dizaine[1]+'-';
       	else numberconverted +=' '+dizaine[decade-1];
-      	alert(numberconverted);
       }
       if (unit)
       {
       	if (decade === 7 || decade === 8 || decade === 9) numberconverted +=' '+special[unit-1];
       	else numberconverted +=' '+digit[unit-1];
-      	a
       }
    }
-   else if (usrnb >10 && usrnb <100)
+      // if usrnb2 digit = 2 
+   else if (usrnb2 >10 && usrnb2 <100)
    {
-   	   if (decade)
-      {
-      	if (decade === 1) ;
-      	if (decade === 7) numberconverted = dizaine[5] +'-';
-      	if (decade === 8 || decade === 9) numberconverted =  digit[3]+'-'+dizaine[1]+'-';
-      	else numberconverted = dizaine[decade-1];
-      }
+       if (decade === 1)numberconverted = "";
+       else if (decade === 7) numberconverted = dizaine[5] +'-';
+       else if (decade === 8 || decade === 9) numberconverted =  digit[3]+'-'+dizaine[1]+'-';
+       else numberconverted = dizaine[decade-1];
+      
       if (unit)
       {
-      	if (decade === 7 || decade === 8 || decade === 9 || decade === 1) numberconverted +=' '+special[unit-1];
+      	if ( decade===1 || decade === 7 || decade === 8 || decade === 9 || decade === 1) numberconverted +=' '+special[unit-1];
       	else numberconverted +=' '+digit[unit-1];
       }
    }
